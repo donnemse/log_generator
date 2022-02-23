736bf11efd9f3d85e5d55df154471d8437338d38
yuganji <yuganji@localhost>
2022-02-24 01:23:17 +0900

    getting logger name on the Logger History by sql join

--- src/main/java/com/igloosec/generator/history/HistoryVO.java
+++ src/main/java/com/igloosec/generator/history/HistoryVO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class HistoryVO {
     private int id;
     private String ip;
+    private String name;
     private int loggerId;
     private int outputId;
     private String type;

8c2fa26917289e4dff92f8494f496d499fab4e16
nowonms@naver.com <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>
2022-04-11 17:24:07 +0900

    refactoring by checkstyle

--- src/main/java/com/yuganji/generator/output/sparrow/TCPSocketServerInstance.java
+++ src/main/java/com/yuganji/generator/output/sparrow/TCPSocketServerInstance.java
@@ -1,19 +1,9 @@
 package com.yuganji.generator.output.sparrow;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import com.yuganji.generator.configuration.ApplicationContextProvider;
 import com.yuganji.generator.queue.QueueService;
-
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -25,23 +15,27 @@ import io.netty.handler.logging.LoggingHandler;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Log4j2
 public class TCPSocketServerInstance {
     private final int port;
     private final int id;
     private ChannelFuture cf;
-    
+
     private EventLoopGroup parentGroup;
     private EventLoopGroup childGroup;
     private final QueueService queueService;
-    
+
     private final int maxBuffer = 200;
     private volatile boolean state = true;
     @Getter
     private final Map<String, ChannelHandlerContext> clients;
-    
+
     private TCPSocketServerHandler handler;
-    
+
     public TCPSocketServerInstance(int id, int port) {
         this.id = id;
         this.port = port;
@@ -72,11 +66,11 @@ public class TCPSocketServerInstance {
             }
         }).start();
     }
-    
+
     public void start() {
         this.parentGroup = new NioEventLoopGroup(10);
         this.childGroup = new NioEventLoopGroup();
-        try{
+        try {
             ServerBootstrap sb = new ServerBootstrap();
             sb.group(this.parentGroup, this.childGroup)
             .channel(NioServerSocketChannel.class)
@@ -84,7 +78,7 @@ public class TCPSocketServerInstance {
             .handler(new LoggingHandler(LogLevel.INFO))
             .childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
-                protected void initChannel(SocketChannel sc) throws Exception {
+                protected void initChannel(SocketChannel sc) {
                     ChannelPipeline p = sc.pipeline();
                     handler = new TCPSocketServerHandler(clients);
                     p.addLast("encoder", new ObjectEncoder());
@@ -97,10 +91,9 @@ public class TCPSocketServerInstance {
             this.cf = sb.bind(this.port).sync();
             // 서버 소켓이 닫힐때까지 대기합니다.
             this.cf.channel().closeFuture().sync();
-        }catch(Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
-        }
-        finally{
+        } finally {
             parentGroup.shutdownGracefully();
             childGroup.shutdownGracefully();
         }
@@ -119,7 +112,7 @@ public class TCPSocketServerInstance {
         this.state = false;
         return true;
     }
-    
+
     public boolean stopClient(String id) {
         if (!this.clients.containsKey(id)) {
             return false;
@@ -128,7 +121,6 @@ public class TCPSocketServerInstance {
             this.clients.get(id).disconnect();
             this.clients.remove(id);
         }
-        
         return true;
     }
 
